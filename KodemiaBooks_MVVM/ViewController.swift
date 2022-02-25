88d9928dfb8edb46932069c131097c5f42aaf83c
Saul <saul.riot@gmail.com>
2022-02-24 19:39:55 -0600

    Initial Commit

---
+++ KodemiaBooks_MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KodemiaBooks_MVVM
+//
+//  Created by Saúl Pérez on 24/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
